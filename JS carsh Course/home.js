dbb9890d5fa7a2c35f6fb19027196e2f85eb622f
KamalSinghKhanna <kamalsinghkhanna@gmail.com>
2022-07-27 13:07:24 +0530

    just a check

--- JS carsh Course/home.js
+++ JS carsh Course/home.js
@@ -1,44 +1,50 @@
-// console.log('hello');
 // // console.log('hello');
-// //alert("yoo");
+// // // console.log('hello');
+// // //alert("yoo");
 
-// // How to wirte a comment inline
+// // // How to wirte a comment inline
 
-// // variable
-// var b = 'smoothie';
-// console.log(b);
+// // // variable
+// // var b = 'smoothie';
+// // console.log(b);
 
-// var aNumber = 45;
-// console.log(aNumber);
-
-
-
-
-// var age = prompt('what is your age ?');
-
-// console.log(age);
-
-// document.getElementById('someText').innerHTML = age;
-
-//Number in js
-var num1 = 5;
-// num1 = num1 + 1;
-num1++;
-num1++;
-num1--;
-// console.log(num1);
+// // var aNumber = 45;
+// // console.log(aNumber);
 
 
-// function greeting() {
-//     var name = prompt("what is your name?");
-//     console.log(`Hello ${name}`);
+
+
+// // var age = prompt('what is your age ?');
+
+// // console.log(age);
+
+// // document.getElementById('someText').innerHTML = age;
+
+// //Number in js
+// var num1 = 5;
+// // num1 = num1 + 1;
+// num1++;
+// num1++;
+// num1--;
+// // console.log(num1);
+
+
+// // function greeting() {
+// //     var name = prompt("what is your name?");
+// //     console.log(`Hello ${name}`);
+// // }
+
+// // greeting();
+
+// const add = (a,b)=>{
+//     console.log(a+ b);
 // }
 
-// greeting();
 
-const add = (a,b)=>{
-    console.log(a+ b);
+// add(4, 5);
+
+function greet(){
+    console.log("hello world")
 }
 
-
-add(4, 5);
+greet()
